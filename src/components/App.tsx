d2bfd6b8550ca254b00b83cc2112bb2aa98446c1
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-08-22 15:21:15 +0200

    creation d'un premier composant Cards

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Cards from './Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.tsx
@@ -0,0 +1,9 @@
+const Cards = () => {
+    return (
+        <div>
+            A apprendre
+        </div>
+    );
+}
+
+export default Cards;
