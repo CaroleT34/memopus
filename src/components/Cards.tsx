f894010aaa6539c1d48af9b30800ea4cd7ea8e79
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-08-22 18:02:24 +0200

    import libraire fontAwesome

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import CardInterface from "../Interface/CardInterface";
+import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Cards = (props: CardInterface): React.JSX.Element => {
   const [showResponse, setShowResponse] = useState(false);
@@ -17,14 +19,30 @@ const Cards = (props: CardInterface): React.JSX.Element => {
         </h6>
         {showResponse && <p className="card-text">{props.answer}</p>}
       </div>
-      <div className="card-footer text-muted">
+      <div className="card-footer d-flex">
         <button
           type="button"
-          className="btn btn-warning"
+          className="btn btn-warning col mx-2"
           title="proposer une réponse"
           //onClick={submitAnswer}
         >
           Proposer une réponse
+        </button>
+        <button
+          type="button"
+          className="btn btn-info col-2 mx-1"
+          title="modifier la carte"
+          //onClick={updateCard}
+        >
+          <FontAwesomeIcon icon={faPencil} />
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger col-2 mx-1"
+          title="supprimer la carte"
+          //onClick={deleteCard}
+        >
+          <FontAwesomeIcon icon={faTrash} />
         </button>
       </div>
     </div>
